818196b62db85a27b36cf56d68a54a5336e3997d
andreiTeodorescu97 <andrei.teodorescu997@gmail.com>
2021-04-05 23:06:27 +0300

    Finalized pacient contact edit page

--- client/src/app/pages/user/user.component.ts
+++ client/src/app/pages/user/user.component.ts
@@ -1,6 +1,6 @@
 import { DatePipe } from '@angular/common';
 import { AbstractType, Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { City } from 'app/_models/city';
 import { Pacient } from 'app/_models/pacient';
 import { Region } from 'app/_models/region';
@@ -23,8 +23,9 @@ export class UserComponent implements OnInit {
     cities: City[];
     citiesReplica: City[];
     regions: Region[];
-    @ViewChild('editForm') editForm : NgForm;
+    /* @ViewChild('editForm') editForm : NgForm; */
     dateFormated: any;
+    editForm: FormGroup;
 
     constructor(private pacientService: PacientService, private accountService: AccountService, private cityService: CityService, private toastr:ToastrService) {
         this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
@@ -34,11 +35,40 @@ export class UserComponent implements OnInit {
         this.getPacient();
         this.getCities();
         this.getRegions();
+        this.initializeForm();
+        this.editForm.get("pacientContact.regionId").valueChanges.subscribe(newRegion => {
+          this.model.pacientContact.cityId = null;
+          this.cities = this.citiesReplica.filter(city => city.regionId == newRegion);
+          this.model.pacientContact.regionId = newRegion;
+          this.editForm.patchValue (this.model, {emitEvent:false});
+        })
     }
+
+    initializeForm() {
+        this.editForm = new FormGroup({
+            firstName: new FormControl('', Validators.required),
+            secondName: new FormControl('', Validators.required),
+            email: new FormControl('', [Validators.required,Validators.pattern('^[^\\s@]+@[^\\s@]+\\.[^\\s@]{2,}$')]),
+            series: new FormControl(),
+            dateOfBirth : new FormControl(),
+            identityNumber: new FormControl(),
+            cnp: new FormControl(),
+            pacientContact: new FormGroup({
+                firstPhone : new FormControl(),
+                secondPhone : new FormControl(),
+                street : new FormControl(),
+                streetNumber : new FormControl(),
+                regionId : new FormControl(),
+                cityId : new FormControl(),
+            })
+        });
+    }
+
     getPacient() {
         this.pacientService.getPacientByCnp(this.user.cnp)
             .subscribe(response => {
                 this.model = response;
+                this.editForm.patchValue (this.model, {emitEvent:false});
                 this.dateFormated = this.model.dateOfBirth;
                 var userdate: any = new Date(this.dateFormated);
                 var datePipe = new DatePipe('en-US')
@@ -60,8 +90,8 @@ export class UserComponent implements OnInit {
     }
 
     updatePacient(){
-        console.log(this.model);
-        this.pacientService.updatePacient(this.model).subscribe(() => {
+        console.log(this.editForm.value);
+        this.pacientService.updatePacient(this.editForm.value).subscribe(() => {
           this.toastr.success(
             '<span data-notify="icon" class="nc-icon nc-bell-55"></span><span data-notify="message">Profilul a fost actualizat cu succes!</span>',
             "Update",
@@ -73,8 +103,12 @@ export class UserComponent implements OnInit {
         })
       }
 
-      regionChange(){
+      regionChange(): void{
         this.model.pacientContact.cityId = null;
         this.cities = this.citiesReplica.filter(city => city.regionId == this.model.pacientContact.regionId);
+      }
+
+      onRegionChange():void {
+
       }
 }
